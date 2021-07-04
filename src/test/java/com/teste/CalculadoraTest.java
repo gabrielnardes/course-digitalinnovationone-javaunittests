6b895c03bc73382c2600f41ff44f00818b7b7ccc
Gabriel Nardes Giampietro <gabrielgiampietro@touchtec.com.br>
2021-07-04 14:16:54 -0300

    Add mock example
    Change-Id: Iaf92b2fa12ba8451f80c3072d2bd4f932b721986

--- src/test/java/com/teste/CalculadoraTest.java
+++ src/test/java/com/teste/CalculadoraTest.java
@@ -1,8 +1,10 @@
 package com.teste;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class CalculadoraTest {
 
@@ -11,5 +13,16 @@ class CalculadoraTest {
         Calculadora calculadora = new Calculadora();
         int soma = calculadora.somar("1+1+3");
         assertEquals(5, soma);
+    }
+
+    @Test
+    public void somarComMock() {
+        Calculadora calculadora = Mockito.mock(Calculadora.class);
+
+        when(calculadora.somar("1+2")).thenReturn(10);
+
+        int resultado = calculadora.somar("1+2");
+
+        assertEquals(10, resultado);
     }
 }
